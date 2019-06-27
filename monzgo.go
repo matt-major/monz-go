c283e6a10ddb7bb2b91bb4f363113015b3775ba9
matt-major <mattmajor14@gmail.com>
2019-06-27 17:45:40 +0100

    Provide support for PUT/POST requests with data

--- monzgo.go
+++ monzgo.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 // Monzgo client
@@ -24,27 +26,56 @@ func (e *MonzoError) Error() string {
 	return fmt.Sprintf("%s: %s", e.Code, e.Message)
 }
 
-func (m *Monzgo) request(requestMethod string, path string, response interface{}, queryParams map[string]string) error {
+func (m *Monzgo) request(requestMethod string, path string, responseHolder interface{}, requestData map[string]string) error {
 	var request *http.Request
 	var err error
 
-	request, err = http.NewRequest(requestMethod, m.BaseURL+path, nil)
+	if requestMethod == "GET" {
+		request, err = m.createRequestWithQuery(path, requestData)
+	} else {
+		request, err = m.createRequestWithFormData(requestMethod, path, requestData)
+	}
+
 	if err != nil {
 		return err
 	}
 
+	return m.performRequest(request, responseHolder)
+}
+
+func (m *Monzgo) createRequestWithQuery(path string, queryParams map[string]string) (*http.Request, error) {
+	request, err := http.NewRequest("GET", m.BaseURL+path, nil)
+	if err != nil {
+		return nil, err
+	}
+
 	if len(queryParams) > 0 {
 		q := request.URL.Query()
-		for k, v := range queryParams {
-			q.Add(k, v)
+		for key, val := range queryParams {
+			q.Add(key, val)
 		}
 		request.URL.RawQuery = q.Encode()
 	}
 
-	return m.makeHTTPrequest(request, response)
+	return request, err
 }
 
-func (m *Monzgo) makeHTTPrequest(request *http.Request, response interface{}) error {
+func (m *Monzgo) createRequestWithFormData(requestMethod string, path string, parameters map[string]string) (*http.Request, error) {
+	formData := url.Values{}
+	for key, val := range parameters {
+		formData.Set(key, val)
+	}
+
+	request, err := http.NewRequest(requestMethod, m.BaseURL+path, strings.NewReader(formData.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	return request, nil
+}
+
+func (m *Monzgo) performRequest(request *http.Request, responseHolder interface{}) error {
 	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", m.APIKey))
 
 	res, err := http.DefaultClient.Do(request)
@@ -70,7 +101,7 @@ func (m *Monzgo) makeHTTPrequest(request *http.Request, response interface{}) er
 		return monzoErr
 	}
 
-	if err := json.Unmarshal(bytes, response); err != nil {
+	if err := json.Unmarshal(bytes, responseHolder); err != nil {
 		return err
 	}
 

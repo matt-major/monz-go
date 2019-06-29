24ed3c934c55a5c7b62cd52984802660ebcd5bba
matt-major <mattmajor14@gmail.com>
2019-06-29 18:18:02 +0100

    Refactor ping.go

--- ping.go
+++ ping.go
@@ -3,9 +3,5 @@ package monzgo
 // Ping the Monzo API to verify the conenction
 func (m *Monzgo) Ping() error {
 	result := &PingResult{}
-	if err := m.request("GET", "ping/whoami", result, nil); err != nil {
-		return err
-	}
-
-	return nil
+	return m.request("GET", "ping/whoami", result, nil)
 }
